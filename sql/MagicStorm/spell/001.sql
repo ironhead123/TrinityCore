a00a48b7a3f646ae175f168679e21da6c0ae111c
welder1976 <zemetskia@gmail.com>
2019-11-07 21:30:14 +0300

    Core: fix Spell Monk Roll
    thanks ReyDonovan

---
+++ sql/MagicStorm/spell/001.sql
@@ -0,0 +1,4 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (109132, 107427);
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(109132, 'spell_monk_roll'),
+(107427, 'spell_monk_roll_trigger');
